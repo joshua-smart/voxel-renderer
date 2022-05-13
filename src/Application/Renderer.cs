2bba49f0ef4320be7106eff93a026b1e765d342b
joshua-smart <josh@thesmarts.co.uk>
2022-05-13 15:33:26 +0100

    Implemented Renderer.cs main Application class

---
+++ src/Application/Renderer.cs
@@ -0,0 +1,23 @@
+using VoxelRenderer.Domain.Interfaces;
+using VoxelRenderer.Application.Interfaces;
+
+namespace VoxelRenderer.Application;
+
+class Renderer {
+
+    private readonly ISceneLoader SceneLoader;
+    private readonly IImageWriter ImageWriter;
+    private readonly ICamera Camera;
+
+    public Renderer(ISceneLoader sceneLoader, IImageWriter imageWriter, ICamera camera) {
+        SceneLoader = sceneLoader;
+        ImageWriter = imageWriter;
+        Camera = camera;
+    }
+
+    public void Render() {
+        IScene loadedScene = SceneLoader.Load();
+        IImage renderedImage = Camera.Render(loadedScene);
+        ImageWriter.Write(renderedImage);
+    }
+}
