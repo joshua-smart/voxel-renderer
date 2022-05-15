b955378f0a88da62e0e8d557a0f03482b5546f33
joshua-smart <josh@thesmarts.co.uk>
2022-05-15 22:47:00 +0100

    Added GetWidth and GetHeight methods

--- src/Domain/Interfaces/IImage.cs
+++ src/Domain/Interfaces/IImage.cs
@@ -5,4 +5,7 @@ public interface IImage {
     public void SetPixel(int x, int y, IColor c);
 
     public IColor GetPixel(int x, int y);
+
+    public int GetWidth();
+    public int GetHeight();
 }
