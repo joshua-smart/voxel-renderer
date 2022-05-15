45e6e461bb30d4cfad2917d19bdd43e2c4f096e0
joshua-smart <josh@thesmarts.co.uk>
2022-05-15 21:51:06 +0100

    Implemented single array, 4byte indexed Image model

--- src/Application/Image.cs
+++ src/Application/Image.cs
@@ -5,17 +5,25 @@ namespace VoxelRenderer.Application.Image;
 
 public class Image : IImage {
 
-    private byte[,,] Pixels;
+    private byte[] Pixels;
+    private int Width;
+    private int Height;
 
     public Image(int width, int height) {
-        Pixels = new byte[width, height, 4];
+        Width = width;
+        Height = height;
+        Pixels = new byte[width * height * 4];
+    }
+
+    private int GetIndex(int x, int y, int i) {
+        return y * Width * 4 + x * 4 + i;
     }
 
     public IColor GetPixel(int x, int y) {
         byte[] data = new byte[4];
 
         for (int i = 0; i < 4; i++) {
-            data[i] = Pixels[x, y, i];
+            data[i] = Pixels[GetIndex(x, y, i)];
         }
 
         return Color.FromBytes(data);
@@ -24,7 +32,7 @@ public class Image : IImage {
     public void SetPixel(int x, int y, IColor c) {
         byte[] bytes = c.ToBytes();
         for (int i = 0; i < 4; i++) {
-            Pixels[x, y, i] = bytes[i];
+            Pixels[GetIndex(x, y, i)] = bytes[i];
         }
     }
 }
