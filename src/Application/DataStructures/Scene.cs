c3634d002def7c636deaa146b6bf51ae2f5f8a49
joshua-smart <josh@thesmarts.co.uk>
2022-05-15 21:59:13 +0100

    Implemented single array indexing in Scene

--- src/Application/DataStructures/Scene.cs
+++ src/Application/DataStructures/Scene.cs
@@ -5,20 +5,29 @@ namespace VoxelRenderer.Application.DataStructures;
 
 public class Scene : IScene {
 
-    private IMaterial[][][] VoxelSpace;
+    private int[] VoxelSpace;
+    private int Width;
+    private int Height;
+    private IMaterial[] Materials;
 
     private Vector3 CameraPosition;
     private Quaternion CameraRotation;
 
-    public Scene(IMaterial[][][] voxelSpace, Vector3 cameraPosition, Quaternion cameraRotation) {
+    public Scene(int[] voxelSpace, IMaterial[] materials, Vector3 cameraPosition, Quaternion cameraRotation) {
         VoxelSpace = voxelSpace;
+        Materials = materials;
         CameraPosition = cameraPosition;
         CameraRotation = cameraRotation;
     }
 
+    private int GetIndex(int x, int y, int z) {
+        return z * Width * Height + y * Width + x;
+    }
+
     public IMaterial? GetVoxel(int x, int y, int z) {
         try {
-            return VoxelSpace[x][y][z];
+            int materialIndex = VoxelSpace[GetIndex(x, y, z)];
+            return Materials[materialIndex];
         } catch (IndexOutOfRangeException e) {
             return null;
         }
